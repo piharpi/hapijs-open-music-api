7139148352a132cb7cfd39774d113fc38610d2a3
Mahendrata Harpi <id.harpi@pm.me>
2023-02-01 16:16:53 +0700

    add new migration create junction table

---
+++ migrations/1675180252788_create-table-playlists-songs.js
@@ -0,0 +1,39 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('playlists_songs', {
+    playlist_id: {
+      type: 'varchar(50)',
+      notNull: true,
+    },
+    song_id: {
+      type: 'varchar(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint(
+    'playlists_songs',
+    'unique_playlist_id_and_song_id',
+    'UNIQUE(playlist_id, song_id)',
+  );
+
+  pgm.addConstraint(
+    'playlists_songs',
+    'fk_playlist_songs.playlist_id_playlist.id',
+    'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE',
+  );
+
+  pgm.addConstraint(
+    'playlists_songs',
+    'fk_playlist_songs.song_id_songs.id',
+    'FOREIGN KEY(song_id) REFERENCES songs(id) ON DELETE CASCADE',
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('playlists_songs');
+  pgm.dropConstraint('playlists_songs');
+  pgm.dropConstraint('fk_playlist_songs.playlist_id_playlists.id');
+  pgm.dropConstraint('fk_playlist_songs.song_id_songs.id');
+};
