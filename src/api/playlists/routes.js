49c6c1f5dcd2b15a6192bb58decbd3f37cb661ac
Mahendrata Harpi <id.harpi@pm.me>
2023-02-01 16:17:21 +0700

    feat: playlist plugin

---
+++ src/api/playlists/routes.js
@@ -0,0 +1,60 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/playlists',
+    handler: (request, h) => handler.postPlaylistHandler(request, h),
+    options: {
+      auth: 'openmusic_api_jwt',
+    },
+  },
+  {
+    method: 'GET',
+    path: '/playlists',
+    handler: (request) => handler.getPlaylistsHandler(request),
+    options: {
+      auth: 'openmusic_api_jwt',
+    },
+  },
+  {
+    method: 'PUT',
+    path: '/playlists/{id}',
+    handler: (request, h) => handler.putPlaylistByIdHandler(request, h),
+    options: {
+      auth: 'openmusic_api_jwt',
+    },
+  },
+  {
+    method: 'DELETE',
+    path: '/playlists/{id}',
+    handler: (request, h) => handler.deletePlaylistByIdHandler(request, h),
+    options: {
+      auth: 'openmusic_api_jwt',
+    },
+  },
+  {
+    method: 'GET',
+    path: '/playlists/{id}/songs',
+    handler: (request, h) => handler.getPlaylistSongsByIdHandler(request, h),
+    options: {
+      auth: 'openmusic_api_jwt',
+    },
+  },
+  {
+    method: 'POST',
+    path: '/playlists/{id}/songs',
+    handler: (request, h) => handler.postPlaylistSongsHandler(request, h),
+    options: {
+      auth: 'openmusic_api_jwt',
+    },
+  },
+  {
+    method: 'DELETE',
+    path: '/playlists/{id}/songs',
+    handler: (request) => handler.deletePlaylistSongByIdHandler(request),
+    options: {
+      auth: 'openmusic_api_jwt',
+    },
+  },
+];
+
+module.exports = routes;
