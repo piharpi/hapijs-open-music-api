b8f192ef8f2b29105097cfdfb04ad70bcf595887
Mahendrata Harpi <id.harpi@pm.me>
2023-02-06 14:26:37 +0700

    feat: albums cover validation

--- src/validator/albums/index.js
+++ src/validator/albums/index.js
@@ -1,9 +1,15 @@
-const { AlbumPayloadSchema } = require('./schema');
+const { AlbumPayloadSchema, AlbumImageHeadersSchema } = require('./schema');
 const InvariantError = require('../../exceptions/InvariantError');
 
 const AlbumValidator = {
   validateAlbumPayload: (payload) => {
     const validationResult = AlbumPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validateAlbumImage: (payload) => {
+    const validationResult = AlbumImageHeadersSchema.validate(payload);
     if (validationResult.error) {
       throw new InvariantError(validationResult.error.message);
     }
