00ef22568cdfc6c936d4f0dd013582f2d76b5f44
Mahendrata Harpi <id.harpi@pm.me>
2023-02-06 18:03:59 +0700

    feat: implement cache service

--- src/server.js
+++ src/server.js
@@ -41,6 +41,7 @@ const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
 
 const StorageService = require('./services/storage/StorageService');
+const CacheService = require('./services/redis/CacheService');
 const AlbumLikesService = require('./services/postgres/AlbumLikesService');
 
 const init = async () => {
@@ -52,7 +53,8 @@ const init = async () => {
   const playlistActivitiesService = new PlaylistActivitiesService();
   const authenticationsService = new AuthenticationsService();
   const storageService = new StorageService(path.resolve(__dirname, 'api/albums/file/images'));
-  const albumLikesService = new AlbumLikesService();
+  const cacheService = new CacheService();
+  const albumLikesService = new AlbumLikesService(cacheService);
 
   const server = Hapi.server({
     host: config.app.host,
